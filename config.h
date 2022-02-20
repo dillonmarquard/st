506ece8588f324ae5f308eadb9648bb932504007
Dillon Marquard <dmarquard4@gmail.com>
2022-02-19 18:55:59 -0800

    changed terminal color pallet

--- config.h
+++ config.h
@@ -95,43 +95,48 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	  /* 8 normal colors */
+	  [0] = "#20201d", /* black   */
+	    [1] = "#d73737", /* red     */
+	      [2] = "#60ac39", /* green   */
+	        [3] = "#cfb017", /* yellow  */
+		  [4] = "#6684e1", /* blue    */
+		    [5] = "#b854d4", /* magenta */
+		      [6] = "#1fad83", /* cyan    */
+		        [7] = "#a6a28c", /* white   */
 
-	[255] = 0,
+			  /* 8 bright colors */
+			  [8]  = "#7d7a68", /* black   */
+			    [9]  = "#d73737", /* red     */
+			      [10] = "#60ac39", /* green   */
+			        [11] = "#cfb017", /* yellow  */
+				  [12] = "#6684e1", /* blue    */
+				    [13] = "#b854d4", /* magenta */
+				      [14] = "#1fad83", /* cyan    */
+				        [15] = "#fefbec", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+					  /* special colors */
+					  [256] = "#20201d", /* background */
+					    [257] = "#a6a28c", /* foreground */
 };
 
+/*
+ *  * Default colors (colorname index)
+ *   * foreground, background, cursor
+ *    */
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 
 /*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+ *  * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ *   * will reverse too. Another logic would only make the simple feature too
+ *    * complex.
+ *     */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
+
 static unsigned int defaultrcs = 257;
 
 /*
